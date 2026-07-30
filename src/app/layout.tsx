edf29b2e1d8b2a34dfffc307993da18e52781e99
Ahmed Tarek <ahmedmasr262@gmail.com>
2026-07-31 00:40:57 +0300

    feat(app): add root layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "../styles/index.css";
+
+export const metadata: Metadata = {
+  title: "Define Visual Direction for Rihla",
+  description:
+    "Rihla is an AI-powered travel companion designed for international tourists in Egypt, offering personalized itineraries and local insights to enhance exploration and ensure a luxurious experience.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <style>{`html, body { height: 100%; margin: 0; } #root { height: 100%; }`}</style>
+      </head>
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
